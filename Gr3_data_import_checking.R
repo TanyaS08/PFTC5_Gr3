2c895c3f9ae308901d97aede88bcf23b6b580b5d
Jonathan Von Oppen <au630524@uni.au.dk>
2020-03-22 17:20:07 +0100

    set up header, imported data

---
+++ Gr3_data_import_checking.R
@@ -0,0 +1,20 @@
+# Plant functional trait course 5
+# Cusco/Wayqecha, Peru - March 2019
+#
+# Group 3: Trait & taxonomic community response to fire and elevation
+# Authors: Dagmar D. Egelkraut, Lucely Vilca Bustamante, Sonya Geange,
+#          Korina Ocampo-Zuleta, Jonathan von Oppen, Jess Rickenback, 
+#          Tanya Strydom
+# Contact: dagmar.egelkraut@uib.no
+
+# 0) Dependencies ----
+library(dplyr)
+
+# 1) Import data ----
+fire_traits <- read.csv("PFTC5_Peru_2020_LeafTraits_cleaned_20-03-21 - PFTC5_Peru_2020_LeafTraits_cleaned_20-03-21.csv", 
+                        header = T, 
+                        sep = ",")
+# 2) Inspect data ----
+
+
+# End of script ----
