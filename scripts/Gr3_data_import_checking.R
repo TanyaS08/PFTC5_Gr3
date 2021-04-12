46a966b752f1dbf1a96687394b4d53526fa743d1
TanyaS08 <tanya.strydom@icloud.com>
2021-04-12 20:41:18 +0200

    all ACJ data now form 2019

--- scripts/Gr3_data_import_checking.R
+++ scripts/Gr3_data_import_checking.R
@@ -2,15 +2,6 @@
 # Cusco/Wayqecha, Peru - March 2020
 #
 # Group 3: Trait & taxonomic community response to fire and elevation
-# Authors: Dagmar D. Egelkraut, Lucely Vilca Bustamante, Sonya Geange,
-#          Korina Ocampo-Zuleta, Jonathan von Oppen, Jess Rickenback,
-#          Tanya Strydom
-# Contact: dagmar.egelkraut@uib.no
-
-
-#' ------------------------------------------------------------------#
-#' TO DO:
-#' ------------------------------------------------------------------#
 
 
 ### 0) Preamble ----
@@ -51,25 +42,27 @@ library(gsheet)
 traits_raw <- read.csv(file.path("data", "raw", "traits", "PFTC3-Puna-PFTC5_Peru_2018-2020_LeafTraits_clean.csv"),
                        header = T,
                        sep = ",") %>%
-  #remove all PFTC3
-  filter(year != 2018)
+  filter(year == 2020) %>%
+  filter(site %in% c("QUE", "TRE")) %>%
+  rbind(read.csv(file.path("data", "raw", "traits", "PFTC3-Puna-PFTC5_Peru_2018-2020_LeafTraits_clean.csv"),
+                 header = T,
+                 sep = ",") %>%
+          filter(year == 2019) %>%
+          filter(site %in% c("QUE", "ACJ")))
 
 skim(traits_raw)
 
 ### >> b) Community data ----
 
-#' I did corrections (cleaning) on the fly here to make sure names and columns match
-#' when importing the QUE data for 2020
-#' 2020 is then combined with the osf datasets after cleaning
-#' this is then followed by some final filtering
-
 species_raw <- read.csv(file.path("data", "raw", "community", "PunaProject_Peru_2019_CommunityCover_clean.csv"),
                  header = T,
                  sep = ",") %>%
+  filter(site %in% c("QUE", "ACJ")) %>%
   rename(species = specie) %>%
   rbind(read.csv(file.path("data", "raw", "community", "PFTC5_Peru_2020_CommunityCover_clean.csv"),
                  header = T,
-                 sep = ","))
+                 sep = ",") %>%
+          filter(site %in% c("QUE", "TRE")))
 
 
 #' This is where the 2020 Community data lives
@@ -90,18 +83,6 @@ species_raw <- read.csv(file.path("data", "raw", "community", "PunaProject_Peru_
 # clean data
 traits <- traits_raw %>%
   
-  ##REMOVE NON-TARGET SITES
-  #selecting the sites we want from PFTC 5
-  filter(site %in% c("ACJ", "QUE", "TRE") & year == 2020 |
-           #Removing ACJ C 2020 
-           site == "ACJ" & year == 2020 & treatment == "NB" |
-           #selecting ACJ C from Puna
-           site == "ACJ" & year == 2019 & treatment== "C" |
-           #QUE form Puna
-           site == "QUE"& year == 2019 |
-           #remove other Puna entries
-           year != 2019) %>%
-  
   ## BURNT QUE SITES FROM 2019 ARE OUR CONTROL SITES
   #Rename 2019 samples to C for QUE
   mutate( 
@@ -114,7 +95,7 @@ traits <- traits_raw %>%
   filter(month != "November",
          #remove Sean's samples
          treatment != "OFF-PLOT",
-         site != "WAY") %>%
+         treatment != "B") %>%
   
   ##REMOVING DUPLICATES FOR INDIVIDUALS
   #group by each individual at each plot for each site & treatment
@@ -132,28 +113,18 @@ skim(traits)
 
 #Combine species datasets
 species <- species_raw %>%
-  
-  ##REMOVE NON-TARGET SITES
-  #selecting the sites we want from PFTC 5
-  filter(site %in% c("ACJ", "QUE", "TRE") & year == 2020 |
-           #Removing ACJ C 2020 
-           site == "ACJ" & year == 2020 & treatment == "NB" |
-           #selecting ACJ C from Puna
-           site == "ACJ" & year == 2019 & treatment== "C" |
-           #QUE form Puna
-           site == "QUE"& year == 2019 |
-           #remove other Puna entries
-           year != 2019) %>%
-  
+
   ## BURNT QUE SITES FROM 2019 ARE OUR CONTROL SITES
   #Rename 2019 samples to C for QUE
   mutate( 
     treatment = case_when(site == "QUE" & 
                             year == 2019 ~ "C",
+                          treatment == "BB" ~ "NB",
                           TRUE ~ treatment)) %>%
   filter(month != "November",
          #remove Sean's samples
-         treatment != "OFF-PLOT")
+         treatment != "OFF-PLOT",
+         treatment != "B")
 
 skim(species)
 
