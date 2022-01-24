b6a8b6d935331e737a1a78cfccca0174067b2b1c
Tanya Strydom <tanya.strydom@icloud.com>
2022-01-24 10:58:49 -0500

    🐣 initial scaffold/outline

---
+++ scripts/DA1_traitstrap.R
@@ -0,0 +1,94 @@
+#' Calculating bootstrapped means using {traitstrap}
+
+### Call source script----
+
+source(here::here(path = "scripts/0_data_import.R"))
+
+### Packages----
+
+# install.packages("remotes")
+remotes::install_github("richardjtelford/traitstrap")
+# library(traitstrap)
+
+species %>%
+  select(taxon, cover, site, treatment, plot_id) %>%
+  mutate(treatment = as.factor(treatment)) %>%
+  str()
+
+### Trait Imputation ----
+
+trait_imputation = 
+  trait_impute(
+    # input data (mandatory)
+    comm = species %>%
+      select(taxon, cover, site, treatment, plot_id) %>%
+      mutate(treatment = as.factor(treatment)),
+    traits = traits %>%
+      select(taxon, trait, value, site, treatment, plot_id) %>%
+      mutate(treatment = as.factor(treatment)),
+    
+    # specifies columns in your data (mandatory)
+    abundance_col = "cover",
+    taxon_col = "taxon",
+    trait_col = "trait",
+    value_col = "value",
+    
+    # specifies sampling hierarchy
+    scale_hierarchy = c("site", "plot_id"),
+    
+    # specifying experimental design
+    treatment_col = "treatment",
+    treatment_level = "site",
+    
+    # min number of samples
+    min_n_in_sample = 3
+  )
+
+# no experimental design
+
+trait_imputation_2 = 
+  trait_impute(
+    # input data (mandatory)
+    comm = species %>%
+      select(taxon, cover, site, treatment, plot_id) %>%
+      mutate(treatment = as.factor(treatment)),
+    traits = traits %>%
+      select(taxon, trait, value, site, treatment, plot_id) %>%
+      mutate(treatment = as.factor(treatment)),
+    
+    # specifies columns in your data (mandatory)
+    abundance_col = "cover",
+    taxon_col = "taxon",
+    trait_col = "trait",
+    value_col = "value",
+    
+    # specifies sampling hierarchy
+    scale_hierarchy = c("site", "treatment", "plot_id"),
+    
+    # min number of samples
+    min_n_in_sample = 3
+  )
+
+# Nonparametric Bootstrapping  ----
+
+bootstrapped_moments = 
+  trait_np_bootstrap(
+    trait_imputation, 
+    nrep = 200
+  )
+
+
+# Summarise Bootstrapping Output  ----
+
+sum_boot_moment <- trait_summarise_boot_moments(
+  np_bootstrapped_moments
+)
+sum_boot_moment
+
+# Some 'random' plots  ----
+
+autoplot(trait_imputation) + 
+  theme(axis.text.x = element_text(size = 8, angle = 90, vjust = 0.5))
+
+autoplot(trait_imputation_2) + 
+  theme(axis.text.x = element_text(size = 8, angle = 90, vjust = 0.5))
