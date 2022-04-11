59a1fc8156f92328d5d60ee5ca4e858dda5e8c7d
Ragnhild Gya <ragnhild.gya@uib.no>
2022-04-11 18:08:08 +0200

    Remove the PCA of the community data - this was not the right method for these data

--- scripts/DA3_Ordinations.R
+++ scripts/DA3_Ordinations.R
@@ -49,46 +49,6 @@ orditorp(nmds, display = "species", col = "red", air = 0.01)
 orditorp(nmds, display = "sites", cex = 1.1, air = 0.01)
 
 
-#### PCA community analysis ####
-# I think we should moe away from this after reading some literature. Seems like the PCA method does not deal with 0 very well, nor is it a good representation of non-linear trends. "PCA is extremely useful when we expect species to be linearly (or even monotonically) related to each other. Unfortunately, we rarely encounter such a situation in nature. It is much more likely that species have a unimodal species response curve" (from: https://ourcodingclub.github.io/tutorials/ordination/)
-
-PCA <- rda(species_wide[-c(1:10)])
-
-plot(PCA)
-plot(PCA, display = c("sites", "species"), type = "points")
-plot(PCA, display = "species", type = "points")
-
-biplot(PCA, choices = c(1,2), type = c("text", "points"), xlim = c(-5,10))
-
-PCA_env <- bind_cols(species_wide[c(1:10)], as.data.frame(PCA$x)) 
-
-PCA_env %>% 
-  ggplot(aes(x = PC1, y = PC2, color = treatment, shape = site)) +  
-  geom_hline(aes(yintercept = 0)) +
-  geom_vline(aes(xintercept = 0)) +
-  geom_point() +
-  stat_ellipse() 
-
-PCA_env %>% 
-  ggplot(aes(x = PC1, y = PC2, color = treatment, shape = site)) +  
-  geom_hline(aes(yintercept = 0)) +
-  geom_vline(aes(xintercept = 0)) +
-  geom_point() +
-  stat_ellipse() +
-  facet_wrap(~year)
-
-PCA_env %>% 
-  ggplot(aes(x = PC1, y = PC2, color = treatment)) +  
-  geom_hline(aes(yintercept = 0)) +
-  geom_vline(aes(xintercept = 0)) +
-  geom_point() +
-  stat_ellipse() +
-  facet_wrap(~site)
-
-autoplot(PCA, data = species_wide, colour = "site",
-         loadings = TRUE, loadings.colour = 'black',
-         loadings.label = TRUE, loadings.label.size = 3, variance_percentage = TRUE)
-
 #### PCA traits analysis ####
 
 # On individual level - not CWM
