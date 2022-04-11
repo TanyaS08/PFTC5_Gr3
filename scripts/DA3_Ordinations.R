ef2c6acca34c198c475efcc52ab8f834563d3fcc
Ragnhild Gya <ragnhild.gya@uib.no>
2022-04-11 18:07:15 +0200

    Adding ordination analysis on trait data (not bootstrapped/community weighted).

--- scripts/DA3_Ordinations.R
+++ scripts/DA3_Ordinations.R
@@ -91,4 +91,52 @@ autoplot(PCA, data = species_wide, colour = "site",
 
 #### PCA traits analysis ####
 
-#Need CWM mean traits from this. Will do later
+# On individual level - not CWM
+
+#### Make wide dataset ####
+
+traits_wide <- traits %>% 
+  #filter(season == "wet_season") %>% 
+  select(-family, -functional_group, -burn_year) %>% 
+  pivot_wider(names_from = trait, values_from = value, values_fill = NA) %>% 
+  filter(!is.na(ldmc),
+         !is.na(leaf_thickness_mm),
+         !is.na(plant_height_cm),
+         !is.na(sla_cm2_g),
+         !is.na(dry_mass_g),
+         !is.na(leaf_area_cm2),
+         !is.na(wet_mass_g)) %>% 
+  mutate(plant_height_cm = log(plant_height_cm),
+         sla_cm2_g = log(sla_cm2_g),
+         leaf_area_cm2 = log(leaf_area_cm2),
+         dry_mass_g = log(dry_mass_g),
+         wet_mass_g = log(wet_mass_g))
+
+#### PCA traits analysis ####
+PCA_trait <- rda(traits_wide[c(14:20)])
+
+plot(PCA_trait$tot.chi)
+
+biplot(PCA_trait, choices = c(1,2), type = c("text", "points"))
+
+PCA_trait_env <- bind_cols(traits_wide[c(1:13)], as.data.frame(PCA_trait$CA$u)) 
+
+#Looking for evidence that there is a difference in traits between wet and dry season - there is not strong evidence for that. We can go on and lump everything together in the dataset and analyse both seasons together.
+# PCA_trait_env %>% 
+#   mutate(site = factor(site, levels = c("ACJ", "TRE", "QUE")))+
+#   ggplot(aes(x = PC1, y = PC2, color = season, shape = treatment)) +  
+#   geom_hline(aes(yintercept = 0)) +
+#   geom_vline(aes(xintercept = 0)) +
+#   geom_point() +
+#   stat_ellipse() +
+#   facet_grid(treatment~site)
+
+PCA_trait_env %>% 
+  mutate(site = factor(site, levels = c("ACJ", "TRE", "QUE"))) %>% 
+  ggplot(aes(x = PC1, y = PC2, color = treatment)) +  
+  geom_hline(aes(yintercept = 0)) +
+  geom_vline(aes(xintercept = 0)) +
+  geom_point() +
+  stat_ellipse() +
+  facet_grid(~site)
+
